33f1888a052616d1b763c291622d9bc91e8e2a62
Mio <85835105+Mioson819@users.noreply.github.com>
2026-04-02 08:45:34 +0700

    rs master Bottom

--- Checktime/getdata.py
+++ Checktime/getdata.py
@@ -1,45 +1,36 @@
 import time
-import pymcprotocol 
-import csv
+from socket import *
+import csv  
 import datetime
+from socketplc import kvHostLink
 
-PLC_IP = "192.168.0.10"
-PLC_PORT = 8501
-CDH = "D718"
-stt = "T90"
-# auto-py-to-exe
+kv = kvHostLink('192.168.0.10')
 
-mc = pymcprotocol.Type3E()
+con_trig = 'DM650'
+count_trig = 'DM23'
+
+c = 'D180.D'
+d = 'D160.D'
+h = 'D170.D'
+time_now = datetime.datetime.now().strftime('%H-%M-%S-%d-%m-%y')
+with open(f'CDH_Bottom_{time_now}.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([f"{time_now}"])    
+        writer.writerow(['Time', 'C', 'D', 'H','C/D','H/D'])
+
+con_trig_value = kv.read(con_trig).decode(errors='ignore')       
+
 while True:
-    try:
-        mc.connect(PLC_IP, PLC_PORT)
-        print("Connected to PLC")
-        break  # Exit the loop if connection is successful
-    except Exception as e:
-        print(f"Connection failed: {e}. Retrying...")
-        time.sleep(0.3)  # Wait before retrying
-        
 
-time.sleep(0.1)
-# Nhập thủ công các giá trị cho data_ng (mặc định 6 word units)
-d1 = 0
-d2 = 0
-counter = 0
-# while True:
-#     data = mc.batchread_wordunits(CDH, 4)
-#     if data[0] != d1 and data[2] != d2:
-#         d1 = data[0]
-#         d2 = data[2]
-#         counter += 1
-#         print(f"Current data at {CDH}: {data[0]}, {data[2]}")
-#     if counter >= 20:
-#         break
-sttplc = mc.batchread_wordunits(stt, 1)
-print(f"Current data at {stt}: {sttplc[0]}")
-
-mc.close()
-print("close socket")
-time.sleep(0.3)
-
-
-
+    count_trig_value = kv.read(count_trig).decode(errors='ignore')
+    #print(f"Con Trig: {int(con_trig_value)}, Count Trig: {int(count_trig_value)}")
+    if int(con_trig_value) == int(count_trig_value):
+        time.sleep(0.15)  # Delay để đảm bảo giá trị đã được cập nhật
+        c_value = kv.read(c).decode(errors='ignore')
+        d_value = kv.read(d).decode(errors='ignore')
+        h_value = kv.read(h).decode(errors='ignore')
+        print(f"Con Trig: {int(con_trig_value)}, Count Trig: {int(count_trig_value)}, C: {float(c_value)}, D: {float(d_value)}, H: {float(h_value)}")
+        with open(f'CDH_Bottom_{time_now}.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([f"{datetime.datetime.now().strftime('%H-%M-%S-%d-%m-%y')}", float(c_value), float(d_value), float(h_value), float(c_value)/float(d_value)*100, float(h_value)/float(d_value)*100])
+        time.sleep(1)  # Delay để tránh ghi quá nhanh vào file CSV
